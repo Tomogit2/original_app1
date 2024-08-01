c6292b432a3469eb215f5b158c3ee3616b74eb24
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-08-01 14:01:13 +0900

    ルーティングの記述を並び替えた。

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'home#index'
 
-  resources :jokes, only: [:create, :show, :destroy]
-  resources :ai_jokes, only: [:show]
+  resources :jokes, only: [:create, :show, :destroy] do
+    resources :ai_jokes, only: [:show]
+  end
+
   resources :users, only: [] do
     resources :jokes, only: [:index], controller: 'user_jokes'
   end
-  root to: 'home#index'
 end
